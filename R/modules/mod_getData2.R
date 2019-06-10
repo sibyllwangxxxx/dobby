f7914dcd45a3c019c92cb471f23407034d1b308d
Bo Wang <bo.wang1@biogen.com>
2019-06-09 22:16:35 -0400

    used col_party in getData2 works

--- R/modules/mod_getData2.R
+++ R/modules/mod_getData2.R
@@ -120,7 +120,7 @@ ui<-fluidPage(
 server<-function(input, output, session){
   data<-callModule(getData2, "getData")
 
-  output$tmp <- renderPrint(data())
+  output$tmp <- renderPrint(col_party(data()))
 
   # output$dt<-renderUI({
   #   lapply(as.list(seq_along(data())), function(i) {
